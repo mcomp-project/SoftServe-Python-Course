6879692c767be8b98ec4e787221478b64f2ee6ff
cache <cache>
2022-09-20 23:10:12 +0300

    added

---
+++ Homework3(Control_Flow_and_Conditionals)/1_Practical_task_from_lecture.py
@@ -0,0 +1,7 @@
+fuelCapacity = int(input('Please enter the fuel capacity in percents(100max)\n'))
+if fuelCapacity >= 10:
+	print(f'The sensor light is green')
+else:
+	print(f'The sensor light is red')
+
+
