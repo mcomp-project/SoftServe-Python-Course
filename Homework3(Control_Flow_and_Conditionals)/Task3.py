f9048499c07f193cfd00e8113c0ae853f85b9bc7
@mcomp-project <kmusiievych@gmail.com>
2022-09-21 22:02:09 +0300

    startedusingPyCharm/addedTask3

---
+++ Homework3(Control_Flow_and_Conditionals)/Task3.py
@@ -0,0 +1,15 @@
+# #The user enters a number, the program must display its description. For example, "positive one-digit",
+# "negative two-digit", etc.
+
+number = int(input(f'Enter the number:\n'))
+digit = len(str(number))  ## len() - returns numbers of "symbols" in variable(100 = 3 digits)
+
+if number < 0:
+    digit -= 1
+
+if number < 0:
+    print(f'{number} is {digit}-digit negative number.\n')
+elif number > 0:
+    print(f'{number} is {digit}-digit positive number.\n')
+else:
+    print(f'Zero is a neutral number.')
