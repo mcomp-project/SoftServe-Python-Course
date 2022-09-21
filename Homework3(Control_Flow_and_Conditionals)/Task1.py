ec9e136cc6f19fa98814a085d906fa0961aeb6bf
@mcomp-project <kmusiievych@gmail.com>
2022-09-21 20:15:29 +0300

    added_Task1

---
+++ Homework3(Control_Flow_and_Conditionals)/Task1.py
@@ -0,0 +1,16 @@
+year = int(input(f'Please enter the year:\n'))
+century = (year//100+1)
+
+if (year % 4 == 0) and (year % 100 != 0) or (year % 400 ==0):
+    print(f'Year {year} is intercalary\n')
+
+elif (year < 1582):
+    print(f'Year must be later than 1582.\n')
+
+elif (year == 1582):
+    print(f'Year {year} is intercalary\n')
+
+else:
+    print(f'Year {year} is non-leap year.\n')
+
+print(f'It`s {century}`th century.\n')
