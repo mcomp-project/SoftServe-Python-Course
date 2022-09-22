6b4e98b58b1c02ce885bd5095fc0bb7928cdd778
@mcomp-project <kmusiievych@gmail.com>
2022-09-22 18:46:58 +0300

    Task5/done

---
+++ Homework3(Control_Flow_and_Conditionals)/Task5.py
@@ -0,0 +1,13 @@
+money = int(input(f'Please enter the amount of money:'))
+
+if money % 200 == 0:
+    mg = money//200
+    print(f"{money} can be exchanged by 200UAH for {mg} times.")
+elif money % 100 == 0:
+    mg = money//100
+    print(f'{money} can be xchanged by 100UAH for {mg} times.')
+elif money % 10 == 0:
+    print(f'{money} can be exchanged by 10UAH for {mg} times.')
+else:
+    mg = money//200, ('200UAH'), (money%200)//100,('100UAH'),((money%200)%100)//10,('10UAH'), ((money%200)%100)%10,('1UAH')
+    print(f'{money} can be exchanged for banknotes such as {mg}')    
