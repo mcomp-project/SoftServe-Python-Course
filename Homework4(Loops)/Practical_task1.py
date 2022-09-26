7164954a82b8684902e0a3693b4ff70987b24cbb
@mcomp-project <kmusiievych@gmail.com>
2022-09-26 15:25:46 +0300

    madePracticalTask1

---
+++ Homework4(Loops)/Practical_task1.py
@@ -0,0 +1,16 @@
+#Write a program that calculates the sum of all even numbers between a and b (inclusive). The values ​​of a and b are entered by the user.
+
+
+# Initialize values A, B and counter "total".
+valueA = float(input(f'Enter number A:\n'))
+valueB = float(input(f'Enter number B:\n'))
+total = 0 
+counter = 0 + valueA 
+	
+while counter <= valueB:
+	if (counter % 2 == 0):
+		print('{0}'.format(counter))
+		total = total + counter
+	counter += 1
+		
+print(f'The Sum of even numbers from {valueA} to {valueB} is {total}')
