c2a26bafc6ade14274e894c73f338d8c73783b58
@mcomp-project <kmusiievych@gmail.com>
2022-09-28 16:35:39 +0300

    PracticalTask1/NotReady

---
+++ Homework5(Collections_Lists_Tuples)/PracticalTask1.py
@@ -0,0 +1,5 @@
+#Find the elements that occur only once in the list, and display them on the screen. That is, find and display unique elements of the array.
+element = []
+for element in range(0,3):
+	element.append(int(input(f'Please input 3 numbers, but one of them input twice:\n')))
+
